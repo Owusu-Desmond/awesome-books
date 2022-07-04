753daf56393a351b4237634f107e4b36113adc65
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-04 15:31:05 +0000

    Add variables module

---
+++ modules/Variables.js
@@ -0,0 +1,20 @@
+// Declare constant variables that are the elments that will be altered universally
+const form = document.getElementById('form');
+const booksContainer = document.querySelector('.added-books-container');
+const main = document.querySelector('main');
+const sections = Array.from(document.querySelectorAll('section'));
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+const bookLists = document.getElementById('books-lists');
+const addNew = document.getElementById('add-new');
+const contact = document.getElementById('contact');
+
+export {
+    form,
+    booksContainer,
+    bookLists,
+    main,
+    sections,
+    navLinks,
+    addNew,
+    contact
+};
